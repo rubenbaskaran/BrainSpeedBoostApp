49b9d9cb829ae3207669f3be2fcdba54db1d5c43
Ruben Baskaran <rbaskaran92@gmail.com>
2018-02-05 10:23:54 +0100

    Color game coming in update

--- app/src/main/java/rubenbaskaran/com/brainchallenge/Menus/MainMenuActivity.java
+++ app/src/main/java/rubenbaskaran/com/brainchallenge/Menus/MainMenuActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import rubenbaskaran.com.brainchallenge.Databases.Managers.LocalDatabaseManager;
 import rubenbaskaran.com.brainchallenge.Enums.GameTypes;
@@ -34,5 +35,6 @@ public class MainMenuActivity extends AppCompatActivity
 
     public void NavigateToColors(View view)
     {
+        Toast.makeText(getApplicationContext(), "Coming in next update!", Toast.LENGTH_SHORT).show();
     }
 }
