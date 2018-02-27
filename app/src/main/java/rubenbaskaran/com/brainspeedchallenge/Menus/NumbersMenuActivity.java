5f61bb801f5aed894dbc23b66f3c4e9f71d0eecc
Ruben Baskaran <rbaskaran92@gmail.com>
2018-02-27 20:29:52 +0100

    Changed MainActivity to NumbersMenuActivity

--- app/src/main/java/rubenbaskaran/com/brainspeedchallenge/Menus/NumbersMenuActivity.java
+++ app/src/main/java/rubenbaskaran/com/brainspeedchallenge/Menus/NumbersMenuActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 
+import rubenbaskaran.com.brainspeedchallenge.Databases.Managers.LocalDatabaseManager;
+import rubenbaskaran.com.brainspeedchallenge.Enums.GameTypes;
 import rubenbaskaran.com.brainspeedchallenge.R;
 
 public class NumbersMenuActivity extends AppCompatActivity
@@ -15,6 +17,13 @@ public class NumbersMenuActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers_menu);
+
+        LocalDatabaseManager localDatabaseManager = new LocalDatabaseManager(getApplicationContext());
+        localDatabaseManager.GetLocalHighscores(GameTypes.Addition);
+        localDatabaseManager.GetLocalHighscores(GameTypes.Subtraction);
+        localDatabaseManager.GetLocalHighscores(GameTypes.Multiplication);
+        localDatabaseManager.GetLocalHighscores(GameTypes.Division);
+        localDatabaseManager.GetLocalHighscores(GameTypes.Color);
     }
 
     public void StartGame(View view)
