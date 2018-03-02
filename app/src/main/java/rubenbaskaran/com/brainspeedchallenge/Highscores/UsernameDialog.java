5df33f18d7b63ba49f94d2ccb9f7b5223df7ba0b
Ruben Baskaran <rbaskaran92@gmail.com>
2018-03-02 20:54:28 +0100

    Removed whitespace above username dialog (on API 21)

--- app/src/main/java/rubenbaskaran/com/brainspeedchallenge/Highscores/UsernameDialog.java
+++ app/src/main/java/rubenbaskaran/com/brainspeedchallenge/Highscores/UsernameDialog.java
@@ -31,11 +31,17 @@ import rubenbaskaran.com.brainspeedchallenge.R;
  * Created by Ruben on 27-02-2018.
  */
 
-// TODO: Why is there a white space above the dialog?
 public class UsernameDialog extends DialogFragment
 {
     SharedPreferences sharedPreferences;
     private InterstitialAd mInterstitialAd;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setStyle(STYLE_NO_TITLE, 0);
+    }
 
     @Nullable
     @Override
